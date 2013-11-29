b3ad79dfc43555ad93ac7429b11a273e2eb777f3
Fábián Gábor <dirtyred.photo@gmail.com>
2013-11-29 18:27:36 +0200

    contact page

--- page.php
+++ page.php
@@ -1,60 +1,80 @@
 <?php get_header(); ?>
 
-			<div id="content">
+<div class="large-12 columns content-header">
+	
+	<h1 class="archive-title xxl" itemprop="headline">
+		<?php the_title(); ?>
+	</h1>
+	
+</div>	
 
-				<div id="inner-content" class="wrap clearfix">
+<div id="main" class="large-8 medium-8 columns" role="main">
 
-						<div id="main" class="eightcol first clearfix" role="main">
+	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+	<article id="post-<?php the_ID(); ?>" <?php post_class( 'clearfix' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
 
-							<article id="post-<?php the_ID(); ?>" <?php post_class( 'clearfix' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
-
-								<header class="article-header">
-
-									<h1 class="page-title" itemprop="headline"><?php the_title(); ?></h1>
-									<p class="byline vcard"><?php
-										printf( __( 'Posted <time class="updated" datetime="%1$s" pubdate>%2$s</time> by <span class="author">%3$s</span>.', 'bonestheme' ), get_the_time( 'Y-m-j' ), get_the_time( __( 'F jS, Y', 'bonestheme' ) ), bones_get_the_author_posts_link());
-									?></p>
-
-
-								</header>
-
-								<section class="entry-content clearfix" itemprop="articleBody">
-									<?php the_content(); ?>
-							</section>
-
-								<footer class="article-footer">
-									<?php the_tags( '<span class="tags">' . __( 'Tags:', 'bonestheme' ) . '</span> ', ', ', '' ); ?>
-
-								</footer>
-
-								<?php comments_template(); ?>
-
-							</article>
-
-							<?php endwhile; else : ?>
-
-									<article id="post-not-found" class="hentry clearfix">
-										<header class="article-header">
-											<h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
-										</header>
-										<section class="entry-content">
-											<p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
-										</section>
-										<footer class="article-footer">
-												<p><?php _e( 'This is the error message in the page.php template.', 'bonestheme' ); ?></p>
-										</footer>
-									</article>
-
-							<?php endif; ?>
-
-						</div>
-
-						<?php get_sidebar(); ?>
-
-				</div>
-
+		<div class="row">
+			<!--
+			<aside class="large-2 medium-2 columns">
+			-->
+				<?php	
+				/*
+				printf(__( '
+					<time class="post-date" datetime="%1$s" pubdate>					
+						<div class="month">%2$s</div>
+						<div class="day h3">%3$s</div>
+						<div class="year">%4$s</div>
+					</time>						
+					', 'bonestheme' 
+					), 
+					get_the_time('Y-m-j'), 
+					get_the_time(__( 'M', 'bonestheme' )), 
+					get_the_time(__( 'j', 'bonestheme' )), 
+					get_the_time(__( 'Y', 'bonestheme' )), 				
+					get_the_category_list(', ')
+				);
+				*/
+				?>
+			<!--
+			</aside>
+			-->
+			
+			<div class="large-12 medium-12 columns">
+				<section class="entry-content clearfix" itemprop="articleBody">
+					<?php the_content(); ?>
+				</section>
 			</div>
+		</div>
+
+		<footer class="article-footer">
+			<?php the_tags( '<span class="tags">' . __( 'Tags:', 'bonestheme' ) . '</span> ', ', ', '' ); ?>
+
+		</footer>
+
+		<?php # comments_template(); ?>
+
+	</article>
+
+	<?php endwhile; else : ?>
+
+			<article id="post-not-found" class="hentry clearfix">
+				<header class="article-header">
+					<h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
+				</header>
+				<section class="entry-content">
+					<p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
+				</section>
+				<footer class="article-footer">
+						<p><?php _e( 'This is the error message in the page.php template.', 'bonestheme' ); ?></p>
+				</footer>
+			</article>
+
+	<?php endif; ?>
+
+</div>
+
+<?php #get_sidebar(); ?>
+
 
 <?php get_footer(); ?>
