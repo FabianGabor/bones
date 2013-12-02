0275f119d3c4baab4159231d39e23673cc9619b8
Fábián Gábor <dirtyred.photo@gmail.com>
2013-12-02 14:18:24 +0200

    small optimizations

--- library/js/vendor/sidenav.js
+++ library/js/vendor/sidenav.js
@@ -9,3 +9,19 @@ jQuery("#sidenav, #menu-toggle").on("click, mouseenter", function(event) {
 
 
 jQuery(document).foundation();
+
+
+// http://www.thecssninja.com/javascript/pointer-events-60fps
+var body = document.body,
+    timer;
+
+window.addEventListener('scroll', function() {
+  clearTimeout(timer);
+  if(!body.classList.contains('disable-hover')) {
+    body.classList.add('disable-hover')
+  }
+  
+  timer = setTimeout(function(){
+    body.classList.remove('disable-hover')
+  },500);
+}, false);
